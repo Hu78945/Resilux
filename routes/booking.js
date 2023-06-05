7be5b98411107d5c60de3abc33b12510b86509a2
Syed Behzad Hassan <behzad789@outlook.com>
2023-06-06 04:17:17 +0500

    minor fix

--- routes/booking.js
+++ routes/booking.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 
 const { createABooking } = require("../controllers/bookinController");
 
-router.route("/").post(createABooking);
+router.route("/:email").post(createABooking);
 
 module.exports = router;
