8204b319a9fe13c40ecc02a48bf32c47d8ae8dc2
vampi-007 <113858174+vampi-007@users.noreply.github.com>
2023-06-06 05:38:42 +0500

    messages Done
    create, Delete andd Get all done

--- index.js
+++ index.js
@@ -19,12 +19,14 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
 const listingRoute = require("./routes/listing");
 const bookingRoute = require("./routes/booking");
+const messageRoute  = require("./routes/messages");
 
 app.use("/api/v1", authRoute);
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/listing", listingRoute);
 app.use("/api/v1/listing", listingRoute);
 app.use("/api/v1/booking", bookingRoute);
+app.use("/api/v1/messages", messageRoute);
 
 app.listen(3000, () => {
   console.log("Server is listing at port 3000");
